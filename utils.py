72f2a9265da0f267528f44c57471c426c526a76c
Mikhail Romanov <mikhail.romanov@aalto.fi>
2023-12-21 14:01:05 +0200

    Added two new util functions

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 # nltk.download('punkt')
 stemmer = PorterStemmer()
 
@@ -12,15 +13,21 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 
+def stem_list(sentence):
+    result = []
+    for w in sentence:
+        result.append(stem(w))
+    return result
+
+
 def bag_of_words(sentence, words):
-    pass
+    sentence_words = stem_list(sentence)
 
-# Testing functions
-a = "How long does shipping take"
-a = tokenize(a)
-print(a)
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words:
+            bag[idx] = 1
 
-b = ["organize", "organization", "organizing"]
-stemmed = [stem(w) for w in b]
-print(stemmed)
+    return bag
+
 
